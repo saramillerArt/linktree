dc43e6b4db220f39b955c47238502960e8f71125
saramillerArt <SaraMiller.S@web.de>
2025-08-06 11:52:43 +0200

    Delete button.js

--- button.js
+++
@@ -1,7 +0,0 @@
-// paypal.js
-paypal.HostedButtons({
-    hostedButtonId: "NRJWCKABZXM2N",
-    onError: function(err) {
-        console.error('Fehler beim Laden des PayPal Buttons:', err);
-    }
-}).render("#paypal-container");
